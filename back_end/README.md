a5b3268e94aa1f8550cb66773c7115cfdab18302
Holden <Holdenkh@gmail.com>
2022-12-02 21:08:27 +0200

    backend

---
+++ back_end/README.md
@@ -0,0 +1,27 @@
+# WAD2022-hw4 Backend
+
+
+## setting up database to test
+Start up a localhost Postgre database with name "testwad" and make sure to add
+your password to the database.js file
+
+
+## Navigate to the project directory
+```
+cd <project name>
+```
+
+## Install dependencies
+```
+npm install
+```
+
+## Run the project
+```
+npm start
+```
+
+or 
+```
+npm server.js
+```
---
+++ back_end/database.js
@@ -0,0 +1,60 @@
+const Pool = require('pg').Pool;
+
+const pool = new Pool({
+    user: "postgres",
+    password: "Lenny123", // need to add your password
+    database: "testwad",
+    host: "localhost",
+    port: "5432"
+});
+
+const execute = async(query) => {
+    try {
+        await pool.connect(); // create a connection
+        await pool.query(query); // executes a query
+        return true;
+    } catch (error) {
+        console.error(error.stack);
+        return false;
+    }
+};
+
+/* 
+gen_random_uuid() A system function to generate a random Universally Unique IDentifier (UUID)
+An example of generated uuid:  32165102-4866-4d2d-b90c-7a2fddbb6bc8
+*/
+
+const createUsersTblQuery = `
+    CREATE TABLE IF NOT EXISTS "users" (
+        id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+        email VARCHAR(200) NOT NULL UNIQUE,
+        password VARCHAR(200) NOT NULL 
+    );`;
+
+
+    const createPostsTblQuery = `
+    CREATE TABLE IF NOT EXISTS "posttable" (
+	    "id" SERIAL PRIMARY KEY,         
+	    "title" VARCHAR(200) NOT NULL,
+	    "body" VARCHAR(200) NOT NULL,
+        "urllink" VARCHAR(200)  
+    );`;
+
+
+
+//creating table for users
+execute(createUsersTblQuery).then(result => {
+    if (result) {
+        console.log('Table "users" is created');
+    }
+});
+
+
+// creating table for posts  
+execute(createPostsTblQuery).then(result => {
+    if (result) {
+        console.log('If does not exists, create the "posttable" table');
+    }
+});
+
+module.exports = pool;
